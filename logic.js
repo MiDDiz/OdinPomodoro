cfe98ad62a747cb088ddb27e0fabba4163599bb5
MiDDiz <36573608+MiDDiz@users.noreply.github.com>
2020-04-19 14:41:01 +0200

    init

---
+++ logic.js
@@ -0,0 +1,4 @@
+//Get documents
+const btnStart = document.getElementById("start_button")
+const btnPause = document.getElementById("start_button")
+const btnMulti = document.getElementById("start_button")
